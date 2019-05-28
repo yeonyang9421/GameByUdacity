d505e0b388d0a3eddfd6e55bdb10521f9a049963
yeonyang9421 <chris09+>
2019-05-28 16:38:08 +0900

    From 1 STEP TO 14 STEP

---
+++ app/src/main/java/kr/co/woobi/imyeon/gamebyudacity/MainActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.woobi.imyeon.gamebyudacity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kr.co.woobi.imyeon.gamebyudacity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController= this.findNavController(R.id.NavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController=this.findNavController(R.id.NavHostFragment)
+        return navController.navigateUp()
+    }
+}
---
+++ app/src/main/java/kr/co/woobi/imyeon/gamebyudacity/TitleFragment.kt
@@ -0,0 +1,31 @@
+package kr.co.woobi.imyeon.gamebyudacity
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import kr.co.woobi.imyeon.gamebyudacity.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false
+        )
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+        return binding.root
+    }
+}
+
+
